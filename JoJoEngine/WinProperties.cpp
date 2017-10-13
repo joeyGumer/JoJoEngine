2c50c5d6a54ade96904ad83c87fd87ff9f13bec1
joeyGumer <pep3553@gmail.com>
2017-10-14 01:15:44 +0200

    Properties Window divided in Transform, Geometry and Textures

--- JoJoEngine/WinProperties.cpp
+++ JoJoEngine/WinProperties.cpp
@@ -1,5 +1,9 @@
 #include "WinProperties.h"
 
+#include "Application.h"
+#include "ModuleRenderer3D.h"
+#include "ModuleFBXLoader.h"
+
 #include "Imgui/imgui.h"
 
 WinProperties::WinProperties()
@@ -22,7 +26,42 @@ void WinProperties::Update()
 	{
 		ImGui::Begin("Properties", &is_open);
 
+		uint size = App->renderer3D->meshes_array.size();
+		if(ImGui::CollapsingHeader("Transform"))
+		{ 
+		
+		}
+		if (ImGui::CollapsingHeader("Geometry"))
+		{
+			uint total_vertices = 0;
+			uint total_faces = 0;
 
+			for (uint i = 0; i < size; i++)
+			{
+				Mesh* tmp_mesh = App->renderer3D->meshes_array[i];
+
+				uint n_vertices = tmp_mesh->num_vertices;
+				uint n_indices = tmp_mesh->num_indices / 3;
+
+				if (ImGui::CollapsingHeader("Mesh "))
+				{
+					ImGui::Text("Num Vertices:  %d", n_vertices);
+					ImGui::Text("Num Triangles: %d", n_indices);
+
+
+				}
+
+				total_vertices += n_vertices;
+				total_faces += n_indices;
+			}
+
+			ImGui::Text("Total vertices: %d", total_vertices);
+			ImGui::Text("Total triangles: %d", total_faces);
+		}
+		if (ImGui::CollapsingHeader("Texture"))
+		{
+
+		}
 
 		ImGui::End();
 	}
